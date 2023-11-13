370957ee88f3997e53a7bc628e4009cbc8dc0236
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2023-11-13 12:33:51 +0200

    firebase configuration

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +19,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+let analytics = null;
+
+if (typeof window !== "undefined") {
+  analytics = getAnalytics(app);
+}
+
+export { analytics };
+// export const analytics = getAnalytics(app);
